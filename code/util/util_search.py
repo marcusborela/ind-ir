5487249bf247f5f2c21f9fa1b17b18736048b89f
borela <borela@tcu.gov.br>
2023-06-09 19:28:58 -0300

    Experiment search execution - doing

--- code/util/util_search.py
+++ code/util/util_search.py
@@ -84,19 +84,17 @@ def detail_document_found(parm_doc_returned):
         for docto in parm_doc_returned:
             print(docto.id, docto.score, docto.meta['name'])
 
-
-
-def calcular_rank1_um_documento (doctos_retornados, id_docto):
+def calcular_rank1_um_documento (docto_found, id_docto):
     """
-    doctos_retornados: lista de documentos retornados na pesquisa (tem id como propriedade)
+    docto_found: lista de documentos retornados na pesquisa (tem id como propriedade)
     id_docto: id do documento relevante
     """
 
-    if len(doctos_retornados)>0:
-        if isinstance(doctos_retornados[0], dict): #doctos_retornados[0]
-            lista_id_docto_retornados = [int(docto['id']) for docto in doctos_retornados]
+    if len(docto_found)>0:
+        if isinstance(docto_found[0], dict): #docto_found[0]
+            lista_id_docto_retornados = [int(docto['id']) for docto in docto_found]
         else: # haystack.schema.Document
-            lista_id_docto_retornados = [int(docto.id) for docto in doctos_retornados]
+            lista_id_docto_retornados = [int(docto.id) for docto in docto_found]
         if id_docto in lista_id_docto_retornados:
             return 1 + lista_id_docto_retornados.index(id_docto) # 1a posição do id_docto na lista
         else:
@@ -104,9 +102,6 @@ def calcular_rank1_um_documento (doctos_retornados, id_docto):
     else:
         # print('não há documentos retornados para id_docto {id_docto}')
         return None
-
-
-
 
 def retorna_parametro(parm_experimento, dados_termo):
     dict_param_busca = {}
@@ -123,15 +118,13 @@ def retorna_parametro(parm_experimento, dados_termo):
         dict_param_busca.update({"Ranker":{"top_k": parm_experimento['topk_ranker']}})
     return dict_param_busca
 
-
-
-def buscar_documentos_para_experimento(parm_experimento, dados_termo):
+def search_docto_for_experiment(parm_experimento, query_data):
     """
     Busca documentos conforme parâmetros
     """
     tamanho_limite = 1024
 
-    if parm_experimento['criterio'] is not None and parm_experimento['tipo_retriever'] == 'tfidf':
+    if parm_experimento['criterio'] is not None and parm_experimento['RETRIEVER_TYPE'] == 'tfidf':
         raise Exception (f'Retriever tfidf não aceita critérios com filtros')
 
     # para evitar erro es.maxClauseCount is set to 1024
@@ -151,101 +144,74 @@ def buscar_documentos_para_experimento(parm_experimento, dados_termo):
 
 
     texto_pesquisa = texto_pesquisa[:tamanho_limite]
-    doctos_retornados = parm_experimento['pipe'].run(query=texto_pesquisa, params=parametro_busca)
+    docto_found = parm_experimento['pipe'].run(query=texto_pesquisa, params=parametro_busca)
 
-    return doctos_retornados
+    return docto_found
 
-
-
-def avalia_pipeline_experimento(parm_df,  parm_experimento,
-                    parm_limite:int=MAIOR_INTEIRO,
-                    parm_se_imprime:bool=False):
+def experiment_run(parm_df,  parm_experiment,
+                    parm_limit_query:int=MAIOR_INTEIRO,
+                    parm_print:bool=False):
     """
-    Cada experimento tem:
-        {'retriever': {'bm25':pipeline_bm25_sem_preproc}, 'base':'vce_relevante_sem_preproc', 'criterio'= uma chave em dict_criterio},
-    Em 9/5/2023
-       mudando search_top_k para: topk_retriever e topk_ranker
+    Consider run as search for all queries
     """
 
-    qtd_dado = min(parm_df.shape[0],parm_limite)
-    fracao_aleatoriedade = qtd_dado/len(parm_df)
+    # param validation
+    list_experiment_keys_expected = ['CRITERIA', 'TOPK_RETRIEVER', 'TOPK_RANKER', 'PIPE', 'DONE']
+    list_pipe_keys_expected = ['PIPE_NAME', 'PIPE_OBJECT', 'RETRIEVER_TYPE', 'RETRIEVER_MODEL_NAME', 'RANKER_MODEL_NAME']
+
+    # Verificar se todas as chaves de list_keys estão em parm_experiment
+    if set(list_experiment_keys_expected) != set(parm_experiment.keys()):
+        raise Exception(f"Invalid keys in parm_experiment {parm_experiment.keys()}. Expected: {list_experiment_keys_expected}")
+
+    # Verificar se todas as chaves de list_keys estão em parm_experiment
+    if set(list_pipe_keys_expected) != set(parm_experiment['PIPE'].keys()):
+        raise Exception(f"Invalid keys in parm_experiment['PIPE'] {parm_experiment['PIPE'].keys()}. Expected: {list_pipe_keys_expected}")
+
+    count_query_run = min(parm_df.shape[0],parm_limit_query)
+    fracao_aleatoriedade = count_query_run/len(parm_df)
     # print(f"fracao_aleatoriedade: {fracao_aleatoriedade}")
     df = parm_df.sample(frac=fracao_aleatoriedade, random_state=123).reset_index(drop=True)
-    resultados = []
-    tempo_inicio = time.time()
-    soma_rank1 = 0
-    qtd_rank1 = 0
-    for cnt, row in tqdm(df.iterrows(), mininterval=10, total=qtd_dado):
-        resultado_termo = {}
-        resultado_termo['cod_termo_vce'] = row['COD_TERMO_VCE']
-        resultado_termo['seq_treino'] = row['SEQ_TREINO']
+    result_search_all_query = []
+    time_start_search_run = time.time()
+    for cnt, row_query in tqdm(df.iterrows(), mininterval=10, total=count_query_run):
+        result_search_one_query = {}
+        result_search_one_query['ID_QUERY'] = row_query['ID']
+        time_start_search_query = time.time()
+        docto_found = search_docto_for_experiment(parm_experiment=parm_experiment,  query_data=row_query)
+        result_search_one_query['COUNT_DOCTO_FOUND'] = len(docto_found['documents'])
+        if len(docto_found) == 0:
+            print(f"\nDocuments not found in experiment {parm_experiment}\nWith parameters: {docto_found['params']}")
+            print(f"With query: {docto_found['query']}")
 
-        tempo_inicio_termo = time.time()
-        doctos_retornados = buscar_documentos_para_experimento(parm_experimento=parm_experimento,  dados_termo=row)
-        resultado_termo['qtd_retornado'] = len(doctos_retornados['documents'])
-        if len(doctos_retornados) == 0:
-            print(f"\nNÃO encontrou documentos em experimento {parm_experimento}\nCom parâmetros: {doctos_retornados['params']}")
-            print(f"Com query: {doctos_retornados['query']}")
-            print(f"Com metadados: Fonte: {row['FONTE']} Sistema: {row['SISTEMA']}")
-        #else:
-        #    print(f"\nNÃO encontrou documentos em experimento {parm_experimento}\nCom parâmetros: {doctos_retornados['params']}")
-        #    print(f"Com query: {doctos_retornados['query']}")
-        #    print(f"Com metadados: {row}")
-
-        resultado_termo['tempo'] = round(time.time() - tempo_inicio_termo, 3)
-        resultado_termo['rank1'] = calcular_rank1_um_documento(doctos_retornados['documents'], int(row['COD_TERMO_VCE']))
-        if resultado_termo['rank1'] is not None:
-            soma_rank1 += resultado_termo['rank1']
-            qtd_rank1 += 1
-        resultados.append(resultado_termo)
-        if cnt >= qtd_dado - 1:
+        result_search_one_query['TIME_SPENT'] = round(time.time() - time_start_search_query, 3)
+        result_search_all_query.append(result_search_one_query)
+        if cnt >= count_query_run - 1:
             break
 
-    tempo_total = time.time() - tempo_inicio
-    dict_retorno = {}
-    dict_retorno['momento'] = time.strftime('%Y-%b-%d %H:%M:%S')
-    dict_retorno['tipo_retriever'] = parm_experimento['tipo_retriever']
-    dict_retorno['tipo_amostra'] = parm_experimento['tipo_amostra']
-    if 'nome_modelo_retriever' in parm_experimento:
-        dict_retorno['nome_modelo_retriever'] = parm_experimento['nome_modelo_retriever']
-    else:
-        dict_retorno['nome_modelo_retriever'] = ""
-    if 'nome_modelo_ranker' in parm_experimento:
-        dict_retorno['nome_modelo_ranker'] = parm_experimento['nome_modelo_ranker']
-    else:
-        dict_retorno['nome_modelo_ranker'] = ""
-    dict_retorno['tipo_base'] = parm_experimento['tipo_base']
-    dict_retorno['qtd_dado'] = qtd_dado
-    dict_retorno['topk_retriever'] = parm_experimento['topk_retriever']
-    dict_retorno['topk_retriever'] = parm_experimento['topk_retriever']
-    if 'topk_ranker' in parm_experimento:
-        dict_retorno['topk_ranker'] = parm_experimento['topk_ranker']
-    else:
-        dict_retorno['topk_ranker'] = ''
+    total_time = time.time() - time_start_search_run
+    result_search_run = {}
+    result_search_run['TIME'] = time.strftime('%Y-%b-%d %H:%M:%S')
+    result_search_run['RETRIEVER_TYPE'] = parm_experiment['PIPE']['RETRIEVER_TYPE']
+    result_search_run['RETRIEVER_MODEL_NAME'] = parm_experiment['PIPE']['RETRIEVER_MODEL_NAME']
+    result_search_run['RANKER_MODEL_NAME'] = parm_experiment['PIPE']['RANKER_MODEL_NAME']
+    result_search_run['COUNT_QUERY_RUN'] = count_query_run
+    result_search_run['TOPK_RETRIEVER'] = parm_experiment['TOPK_RETRIEVER']
+    result_search_run['TOPK_RANKER'] = parm_experiment['TOPK_RANKER']
 
-    if qtd_rank1 > 0:
-        dict_retorno['rank1_mean'] = round(soma_rank1 / qtd_rank1)
-    else:
-        dict_retorno['rank1_mean'] = None
-    dict_retorno['qtd_encontrado'] = qtd_rank1
-    dict_retorno['qtd_nao_encontrado'] = qtd_dado - qtd_rank1
-    dict_retorno['percent_nao_encontrado'] = round(100*dict_retorno['qtd_nao_encontrado']/qtd_dado)
-    dict_retorno['tempo_medio_texto'] = round(tempo_total/qtd_dado,3)
-    if parm_experimento['criterio'] is None:
-        dict_retorno['criterio'] = ""
-    else:
-        dict_retorno['criterio'] = parm_experimento['criterio']
-    dict_retorno['metrica_termos'] = resultados
+    result_search_run['COUNT_QUERY_RUN'] = count_query_run
+    result_search_run['TIME_SPENT_MEAN'] = round(total_time/count_query_run,3)
+    result_search_run['CRITERIA'] = parm_experiment['CRITERIA']
+    result_search_run['RESULT_QUERY'] = result_search_all_query
 
-    if parm_se_imprime:
-        for conjunto in dict_retorno:
-            for metric in dict_retorno[conjunto]:
-                if isinstance(dict_retorno[conjunto][metric], str) or isinstance(dict_retorno[conjunto][metric], int):
-                    print(f"{conjunto:>8}: {metric:>17}: {dict_retorno[conjunto][metric]}")
+    if parm_print:
+        for conjunto in result_search_run:
+            for metric in result_search_run[conjunto]:
+                if isinstance(result_search_run[conjunto][metric], str) or isinstance(result_search_run[conjunto][metric], int):
+                    print(f"{conjunto:>8}: {metric:>17}: {result_search_run[conjunto][metric]}")
                 else:
-                    print(f"{conjunto:>8}: {metric:>17}: {dict_retorno[conjunto][metric]:.5f}")
+                    print(f"{conjunto:>8}: {metric:>17}: {result_search_run[conjunto][metric]:.5f}")
 
-    return dict_retorno
+    return result_search_run
 
 
 
@@ -257,16 +223,16 @@ def add_experiment_result(parm_list_result, parm_path_experiment, parm_path_expe
     df_experiment_result = pd.DataFrame()
 
     # Itere sobre cada linha da coluna METRICA_TERMOS e adicione ao dataframe de saída
-    for _, row in df_experiment.iterrows():
-        momento = row['momento']
-        metricas = row['metrica_termos']
+    for _, row_query in df_experiment.iterrows():
+        TIME = row_query['TIME']
+        metricas = row_query['metrica_termos']
         for metrica in metricas:
-            metrica['momento'] = momento
+            metrica['TIME'] = TIME
             df_experiment_result = df_experiment_result.append(metrica, ignore_index=True)
     del df_avaliacao['metrica_termos']
     df_experiment_result['cod_termo_vce'] = df_experiment_result['cod_termo_vce'].astype(int)
     df_experiment_result['seq_treino'] = df_experiment_result['seq_treino'].astype(int)
-    df_experiment_result['qtd_retornado'] = df_experiment_result['qtd_retornado'].astype(int)
+    df_experiment_result['COUNT_DOCTO_FOUND'] = df_experiment_result['COUNT_DOCTO_FOUND'].astype(int)
     # df_experiment_result['qtd_relevante_encontrado'] = df_experiment_result['qtd_relevante_encontrado'].astype(int)
     # df_experiment_result['qtd_relevante_topk'] = df_experiment_result['qtd_relevante_topk'].astype(int)
 
