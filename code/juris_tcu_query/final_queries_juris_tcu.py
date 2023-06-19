1e7329a0c5c49a19b10223a5430f32746610cf12
leonardo3108 <leonardo3108@gmail.com>
2023-06-19 15:40:23 -0300

    enumerando as queries

--- code/juris_tcu_query/final_queries_juris_tcu.py
+++ code/juris_tcu_query/final_queries_juris_tcu.py
@@ -36,11 +36,13 @@ for line in open('data/llm_juris_tcu/query_llm_selecionada.txt', encoding='Utf-8
         print('?', line)
         assert(False)
 print('questions:', len(questions), 'expressions:', len(expressions), 'statements:', total_statements)
-with open('data/juris_tcu/query2.txt', 'w') as f_out:
-    for question in questions[:50]:
-        f_out.write(question + '\n')
-with open('data/juris_tcu/query3.txt', 'w') as f_out:
-    for expression in expressions[:50]:
-        f_out.write(expression + '\n')
+with open('data/juris_tcu/query2.csv', 'w') as f_out2:
+    f_out2.write('id;text\n')    
+    for i, expression in enumerate(expressions[:50]):
+        f_out2.write(str(51 + i) + ';' + expression + '\n')
+with open('data/juris_tcu/query3.csv', 'w') as f_out3:
+    f_out3.write('id;text\n')    
+    for i, question in enumerate(questions[:50]):
+        f_out3.write(str(101 + i) + ';' + question + '\n')
         
     
