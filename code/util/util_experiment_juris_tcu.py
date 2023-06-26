d3ba74e9575d4e007a0e5cd305807b224daf264b
borela <borela@tcu.gov.br>
2023-06-26 20:03:40 -0300

    search juris_tcu code in test

--- code/util/util_experiment_juris_tcu.py
+++ code/util/util_experiment_juris_tcu.py
@@ -67,7 +67,11 @@ def calculate_ndcg_query_result (list_id_doc_returned, dict_doc_relevant:dict, p
         for i, docid in enumerate(list_id_doc_returned[:position]):
             if i >= position:
                 raise ValueError('Logic error: more than position????')
-            relevance = 1 if docid in dict_doc_relevant else 0
+            # relevance = 1 if docid in dict_doc_relevant else 0
+            if docid in dict_doc_relevant:
+                relevance = dict_doc_relevant[docid]
+            else:
+                relevance = 0
             dcg += (2 ** relevance - 1) / math.log2(i + 2)
 
         # if len(dict_doc_relevant) not in dict_idcg_relevance_fixed:
@@ -85,8 +89,6 @@ def calculate_list_rank_query_result (list_id_doc_returned, dict_doc_relevant:di
     dict_doc_relevant: dict of relevance where id_doc is the key and type relevance (not used) value
     return min (and mean) rank  of relevant docto
     """
-
-
     if list_id_doc_returned is not None and len(list_id_doc_returned) > 0:
         list_rank = []
         for doc_id in dict_doc_relevant:
@@ -98,18 +100,11 @@ def calculate_list_rank_query_result (list_id_doc_returned, dict_doc_relevant:di
 
 def build_search_parameter(parm_experiment, query_data):
     dict_param_busca = {}
-    criteria_type = dict_criterio[parm_experiment['CRITERIA']]['type']
-    criteria_value = dict_criterio[parm_experiment['CRITERIA']]['value']
     if  'join' in parm_experiment['PIPE']['RETRIEVER_TYPE']:
-        dict_param_busca = {"Bm25Retriever":{"top_k": np.int(parm_experiment['TOPK_RETRIEVER']/2), "filters": criteria_value},
-                            "StsRetriever": {"top_k": np.int(parm_experiment['TOPK_RETRIEVER']/2), "filters": criteria_value}}
-    elif parm_experiment['PIPE']['RETRIEVER_TYPE'] in ['bm25','sts','sts_multihop']:
-        if criteria_type == "static":
-            dict_param_busca = {"Retriever":{"top_k": parm_experiment['TOPK_RETRIEVER'], "filters": criteria_value}}
-        else:
-            raise Exception (f"Invalid criteria_type  {criteria_type} in build_search_parameter")
+        dict_param_busca = {"Bm25Retriever":{"top_k": np.int(parm_experiment['TOPK_RETRIEVER']/2)},
+                            "StsRetriever": {"top_k": np.int(parm_experiment['TOPK_RETRIEVER']/2)}}
     else:
-        raise Exception (f"Invalid parm_experiment['PIPE']['RETRIEVER_TYPE']  {parm_experiment['PIPE']['RETRIEVER_TYPE'] }")
+        dict_param_busca = {"Retriever":{"top_k": parm_experiment['TOPK_RETRIEVER']}}
     if 'TOPK_RANKER' in parm_experiment and parm_experiment['TOPK_RANKER'] > 0:
         dict_param_busca.update({"Ranker":{"top_k": parm_experiment['TOPK_RANKER']}})
     return dict_param_busca
@@ -119,8 +114,6 @@ def search_docto_for_experiment(parm_experiment, query_data):
     Busca documentos conforme parâmetros
     """
     limit_size_text_first_stage = 1024
-    if parm_experiment['CRITERIA'] is not None and parm_experiment['PIPE']['RETRIEVER_TYPE'] == 'TFIDF':
-        raise Exception (f'Retriever tfidf do not combine with filters')
     search_parameter = build_search_parameter(parm_experiment, query_data)
     # print(f"search_parameter: {search_parameter} ")
     search_text =  query_data['TEXT'][:limit_size_text_first_stage]
@@ -141,32 +134,20 @@ def search_docto_for_experiment(parm_experiment, query_data):
     else:
         return None
 
-def calculate_ground_truth(parm_criteria:str, parm_dict:dict)->dict:
-    if "total" in parm_criteria or "class_termo" in parm_criteria:
-        return parm_dict
-    # Considera os valores do tipo de indexação armazenados
-    elif parm_criteria == "area":
-        return {key: value for key, value in parm_dict.items() if value == 'AREA'}
-    elif parm_criteria == "theme":
-        return {key: value for key, value in parm_dict.items() if value == 'TEMA'}
-    elif parm_criteria == "subtheme":
-        return {key: value for key, value in parm_dict.items() if value == 'SUBTEMA'}
-    elif parm_criteria == "extra":
-        return {key: value for key, value in parm_dict.items() if value == 'INDEXACAO_EXTRA'}
-    else:
-        raise Exception(f"Invalid parm_criteria {parm_criteria} in calculate_ground_truth")
-
-def experiment_run(parm_df,  parm_experiment,
-                    parm_limit_query:int=GREATEST_INTEGER,
-                    parm_ndcg_position:int=12,
-                    parm_print:bool=False):
+def experiment_run(parm_df,
+                   parm_experiment,
+                   parm_index_name:str, # just for log
+                   parm_limit_query:int=GREATEST_INTEGER,
+                   parm_ndcg_position:int=12,
+                   parm_print:bool=False):
     """
     Consider run as search for all queries
     """
 
     # param validation
-    list_experiment_keys_expected = ['CRITERIA', 'TOPK_RETRIEVER', 'TOPK_RANKER', 'PIPE', 'DONE']
-    list_pipe_keys_expected = ['PIPE_NAME', 'PIPE_OBJECT', 'RETRIEVER_TYPE', 'RETRIEVER_MODEL_NAME', 'RANKER_MODEL_NAME']
+    list_experiment_keys_expected = ['TOPK_RETRIEVER', 'TOPK_RANKER', 'PIPE','DONE']
+    list_pipe_keys_expected = ['PIPE_OBJECT', 'RETRIEVER_TYPE', 'RETRIEVER_MODEL_NAME',
+                               'RANKER_MODEL_NAME', 'RANKER_TYPE']
 
     # Verificar se todas as chaves de list_keys estão em parm_experiment
     if set(list_experiment_keys_expected) != set(parm_experiment.keys()):
@@ -176,20 +157,16 @@ def experiment_run(parm_df,  parm_experiment,
     if set(list_pipe_keys_expected) != set(parm_experiment['PIPE'].keys()):
         raise Exception(f"Invalid keys in parm_experiment['PIPE'] {parm_experiment['PIPE'].keys()}. Expected: {list_pipe_keys_expected}")
 
-    if parm_limit_query==100:
-        PATH_VALID_DATA = f"../data/train_juris_tcu_index/juris_tcu_index_validation_query_id.csv"
-        df_validation = pd.read_csv(PATH_VALID_DATA)
-        list_query_id_valid = df_validation['QUERY_ID'].to_list()
-        df = parm_df[parm_df['ID'].isin(list_query_id_valid)]
-        print(f"Experimento envolverá {df.shape[0]} registros, queries selecionadas para validação")
-    elif parm_limit_query<parm_df.shape[0]:
-        randomness_fraction = count_query_run/len(parm_df)
-        print(f"Experimento envolverá {df.shape[0]} registros com randomness_fraction: {randomness_fraction}")
+    if parm_limit_query<parm_df.shape[0]:
+        randomness_fraction = parm_limit_query/len(parm_df)
         df = parm_df.sample(frac=randomness_fraction, random_state=123).reset_index(drop=True)
+        print(f"Experimento envolverá {df.shape[0]} registros com randomness_fraction: {randomness_fraction}")
     else:
-        print(f"Experimento envolverá {df.shape[0]} registros, toda a base de dados")
         df = parm_df
+        print(f"Experimento envolverá {df.shape[0]} registros, toda a base de dados")
+
     count_query_run = min(df.shape[0],parm_limit_query)
+
     result_search_all_query = []
     total_rank1 = 0 #
     total_ndcg = 0
@@ -217,18 +194,9 @@ def experiment_run(parm_df,  parm_experiment,
             result_search_one_query['LIST_DOCTO_RETURNED'] = ""
             total_without_result += 1
         else:
-            result_search_one_query['LIST_DOCTO_RETURNED'] = str(list_id_doc_returned)
-            dict_ground_truth = calculate_ground_truth(parm_criteria=parm_experiment['CRITERIA'], parm_dict=row_query['RELEVANCE_DICT'])
+            dict_ground_truth = row_query['RELEVANCE_DICT']
             if len(dict_ground_truth) == 0:
-                if parm_experiment['CRITERIA'] != 'extra':
-                    raise Exception (f"no value for calculate_ground_truth, but parm_experiment['CRITERIA'] {parm_experiment['CRITERIA']} != 'extra'")
-                # pode não haver ground truth dependendo do critério, por exemplo, não haver indexação extra
-                result_search_one_query['RANK1'] = 0
-                result_search_one_query['NDCG'] = 0
-                result_search_one_query['COUNT_DOCTO_FOUND'] = 0
-                result_search_one_query['COUNT_DOCTO_RELEVANT'] = 0
-                result_search_one_query['LIST_RANK'] = ""
-                total_not_found += 1
+                raise Exception(f"Not expected query {row_query['ID']} without ground_truth")
             else:
                 list_rank = calculate_list_rank_query_result(list_id_doc_returned, dict_ground_truth)
                 if list_rank is None or len(list_rank) == 0:
@@ -246,6 +214,7 @@ def experiment_run(parm_df,  parm_experiment,
                     result_search_one_query['RANK1'] = min(list_rank)
                     result_search_one_query['LIST_RANK'] = str(list_rank)
                     total_found += 1
+            result_search_one_query['LIST_DOCTO_RETURNED'] = str(list_id_doc_returned)
         # convertendo campos do tipo lista em string
 
         total_rank1 += result_search_one_query['RANK1']
@@ -270,7 +239,6 @@ def experiment_run(parm_df,  parm_experiment,
     result_search_run['NDCG_MEAN'] = round(total_ndcg/count_query_run,3)
     result_search_run['NDCG_LIMIT'] = parm_ndcg_position
     result_search_run['TIME_SPENT_MEAN'] = round(total_time/count_query_run,3)
-    result_search_run['CRITERIA'] = parm_experiment['CRITERIA']
     result_search_run['RETRIEVER_MODEL_NAME'] = parm_experiment['PIPE']['RETRIEVER_MODEL_NAME']
     result_search_run['RANKER_MODEL_NAME'] = parm_experiment['PIPE']['RANKER_MODEL_NAME']
     result_search_run['RESULT_QUERY'] = result_search_all_query
@@ -422,25 +390,10 @@ def add_experiment_result(parm_list_result, parm_dataset):
 
         df_experiment_result.to_csv(path_search_result, sep = ',', index=False)
 
-dict_criterio = {
-    "class_termo" : {"type": "static",
-               "value": {'class':['Termo']}},
-    "total" : {"type": "static",
-                  "value": {"count_index_total": {"$gte": 1} }},
-    "area" : {"type": "static",
-                   "value": {"count_index_area": {"$gte": 1} }},
-    "theme" : {"type": "static",
-                   "value": {"count_index_theme": {"$gte": 1} }},
-    "subtheme" : {"type": "static",
-                   "value": {"count_index_subtheme": {"$gte": 1} }},
-    "extra" : {"type": "static",
-                   "value": {"count_index_extra": {"$gte": 1} }},
-    "total_gte_5" : {"type": "static",
-                  "value": {"count_index_total": {"$gte": 5} }},
-}
 
-dict_idcg_relevance_fixed = generate_dict_idcg(15)
 
+dict_idcg_relevance_fixed = generate_dict_idcg(50)
+print("calculated dict_idcg until @50")
 # print('dict_idcg_relevance_fixed', dict_idcg_relevance_fixed)
 
 
